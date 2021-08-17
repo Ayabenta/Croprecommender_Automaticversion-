d420f6e7f3f64a17d1d0d4f3df26f967e2f0fcbc
Ayabenta <75723449+Ayabenta@users.noreply.github.com>
2021-08-17 14:13:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ auth = firebase.auth()
 database = firebase.database()
 
 #load model 
-model = pickle.load(open('recommendation_plantes.pkl', 'rb'))
+model = pickle.load(open('recommendation_de_plantes.pkl', 'rb'))
 #Create App
 app = Flask(__name__)
 @app.route('/', methods = ['GET'])
